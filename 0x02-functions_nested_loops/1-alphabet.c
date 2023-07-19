a90e69a3a7d3672d25dc6b617f917163787fd177
PhineasImaana <phineasmariojr@gmail.com>
2023-07-18 22:17:02 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet -Make the alphabet
+ *
+ * Retun:void
+ */
+
+void printalphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+putchar('\n');
+}
